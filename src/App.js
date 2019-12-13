abbaed210e746584238ec02e59f9dcc4e180f76b
mark-mansfield <markgmansfield@gmail.com>
2019-12-14 09:56:06 +1100

    added todo list for basic project

--- src/App.js
+++ src/App.js
@@ -3,6 +3,19 @@ import { TimelineMax } from 'gsap/all';
 import './App.css';
 
 const App = props => {
+  /**
+   * * TODO
+   * * 1. set up animating state
+   * * 2. set up refs for props objects ( ie each animation object) to be used in login
+   * * 3. set up refs for svg in the DOM
+   * * set up timelines in state
+   * * set up animation definitions
+   * * add in logic
+   * * write dom tests
+   */
+
+  const [rootTimeline] = useState(new TimelineMax({ paused: true }));
+
   // set up timelines on initial render
   useEffect(() => {
     console.warn('setting up initial timeline');
