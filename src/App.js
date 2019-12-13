b98ea5f33a36ce50cfec32fdc4dff55da905d193
mark-mansfield <markgmansfield@gmail.com>
2019-12-14 09:34:03 +1100

    added useEffec Hooks for timlein and detecting change in props

--- src/App.js
+++ src/App.js
@@ -2,7 +2,17 @@ import React, { useEffect, useState, useRef } from 'react';
 import { TimelineMax } from 'gsap/all';
 import './App.css';
 
-const App = () => {
+const App = props => {
+  // set up timelines on initial render
+  useEffect(() => {
+    console.warn('setting up initial timeline');
+  }, []);
+
+  // detect changes in props
+  useEffect(() => {
+    console.warn('props changed');
+  }, [props]);
+
   return (
     <div className="App">
       <h1>App works!</h1>
